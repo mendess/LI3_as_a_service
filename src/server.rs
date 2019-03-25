2ba8a942f325097f8855646153df2ed579ee0e77
Mendess2526 <pedro.mendes.26@gmail.com>
2019-03-25 18:19:00 +0000

    Remove query from API

--- src/server.rs
+++ src/server.rs
@@ -14,22 +14,22 @@ use rocket::State;
 #[get("/")]
 fn index() -> &'static str {
 "   Welcome to the LI3 API!
-    /query2/<leter>
-    /query3/<month>/<client>
-    /query4
-    /query4/<filial>
-    /query5
-    /query6
-    /query7/<client>
-    /query8/<from>/<to>
-    /query9/<product>/<filial>/<promotion>
-    /query10
-    /query11/<n>
-    /query12
+    /2/<leter>
+    /3/<month>/<client>
+    /4
+    /4/<filial>
+    /5
+    /6
+    /7/<client>
+    /8/<from>/<to>
+    /9/<product>/<filial>/<promotion>
+    /10
+    /11/<n>
+    /12
     "
 }
 
-#[get("/query2/<leter>")]
+#[get("/2/<leter>")]
 fn query2(store: State<Store>, leter: String) -> String {
     let l = store.list_by_first_letter(leter.chars().next().unwrap());
     let mut response = String::new();
@@ -39,13 +39,13 @@ fn query2(store: State<Store>, leter: String) -> String {
     format!("{}", response)
 }
 
-#[get("/query3/<month>/<client>")]
+#[get("/3/<month>/<client>")]
 fn query3(store: State<Store>, month: u8, client: String) -> String {
     let l = store.total_billed(Month::from(month), client);
     format!("{:#?}", l)
 }
 
-#[get("/query4")]
+#[get("/4")]
 fn query4(store: State<Store>) -> String {
     let l = store.never_bought();
     let mut response = String::new();
@@ -56,12 +56,12 @@ fn query4(store: State<Store>) -> String {
     format!("{}", response)
 }
 
-#[get("/query4/<filial>")]
+#[get("/4/<filial>")]
 fn query4_filial(_store: State<Store>, filial: u8) -> String {
     format!("Not done yet!\n")
 }
 
-#[get("/query5")]
+#[get("/5")]
 fn query5(store: State<Store>) -> String {
     let l = store.buyers_in_all_filials();
     let mut response = String::new();
@@ -71,14 +71,14 @@ fn query5(store: State<Store>) -> String {
     format!("{}", response)
 }
 
-#[get("/query6")]
+#[get("/6")]
 fn query6(store: State<Store>) -> String {
     let buyers = store.n_buyers_without_purchases();
     let products = store.n_never_bought();
     format!("clients: {} | products: {}", buyers, products)
 }
 
-#[get("/query7/<client>")]
+#[get("/7/<client>")]
 fn query7(store: State<Store>, client: String) -> String {
     let table = store.year_purchases(client);
     let mut response = String::new();
@@ -102,12 +102,12 @@ fn query7(store: State<Store>, client: String) -> String {
     response
 }
 
-#[get("/query8/<from>/<to>")]
+#[get("/8/<from>/<to>")]
 fn query8(store: State<Store>, from: u8, to: u8) -> String {
     format!("{:?}", store.total_billed_between(Month::from(from), Month::from(to)))
 }
 
-#[get("/query9/<product>/<filial>/<promotion>")]
+#[get("/9/<product>/<filial>/<promotion>")]
 fn query9(store: State<Store>, product: String, filial: u8, promotion: bool) -> String {
     let l = store.product_buyers(&product, store::sale::Filial::from(filial), promotion);
     let mut response = String::new();
@@ -117,12 +117,12 @@ fn query9(store: State<Store>, product: String, filial: u8, promotion: bool) -> 
     format!("{}", response)
 }
 
-#[get("/query10")]
+#[get("/10")]
 fn query10(_store: State<Store>) -> String {
     format!("Not done yet!")
 }
 
-#[get("/query11/<n>")]
+#[get("/11/<n>")]
 fn query11(store: State<Store>, n: usize) -> String {
     let l = store.top_sold_products(n, false);
     let mut response = String::new();
@@ -132,14 +132,14 @@ fn query11(store: State<Store>, n: usize) -> String {
     format!("{}", response)
 }
 
-#[get("/query12")]
+#[get("/12")]
 fn query12(_store: State<Store>) -> String {
     format!("Not done yet!\n")
 }
 
 #[catch(404)]
 fn catch404(_req: &Request) -> String {
-    format!("Not a valid query!")
+    format!("Not a valid !")
 }
 
 fn main() -> std::io::Result<()>{
